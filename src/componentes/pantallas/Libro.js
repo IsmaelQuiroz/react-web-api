9bf6576fc7f658479af665c37599fd1c70c0aa8c
Ismael Quiroz Santes <ismaelqzs@gmail.com>
2026-01-20 09:22:52 -0600

    controlamiento del formulario

--- src/componentes/pantallas/Libro.js
+++ src/componentes/pantallas/Libro.js
@@ -1,12 +1,46 @@
-import React from 'react';
-import { Button, Card, Container, Grid, MenuItem, TextField, Typography } from '@material-ui/core';
+import React, { useState } from 'react';
+import { Button, Card, Container, Grid, MenuItem, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField, Typography } from '@material-ui/core';
 import useStyles from '../../theme/useStyles';
 
+const clearLibro = {
+    categoria : '',
+    titulo: '',
+    autor: ''
+}
+
 const Libro = () => {
+
+    const [libro, setLibro] = useState({
+        categoria : '',
+        titulo: '',
+        autor: ''
+    });
+
+    const handleChange = (e) => {
+        const {name, value} = e.target;
+        setLibro(prev => ({
+            ...prev,
+            [name] : value
+        }))
+    }
+
+    const guardarData =() =>{
+        console.log("Mis datos son", libro);
+        setLibro(clearLibro);
+    }
+
+    const abrirDialog = () =>{
+        console.log("mi boton editar");
+    }
+
+    const eliminarData = () =>  {
+        console.log("mi boton eliminar");
+    }
+
     const classes = useStyles();
     return(
        <Container className={classes.containermt}>
-            <Grid container justify="center">
+            <Grid container justifyContent="center">
                <Grid item lg={7} md={8}>
                 <Card className = {classes.card} align="center">
                     <Typography variant="h4">Libros</Typography>
@@ -18,7 +52,10 @@ const Libro = () => {
                                 label="Categoria"
                                 variant="outlined"
                                 fullWidth
-                                align="left">
+                                align="left"
+                                name="categoria"
+                                value={libro.categoria}
+                                onChange={handleChange}>
                                     <MenuItem value="Programacion">Programacion</MenuItem>
                                     <MenuItem value="Historia">Historia</MenuItem>
                                     <MenuItem value="Matematica">Matematica</MenuItem>
@@ -29,6 +66,9 @@ const Libro = () => {
                                 label="Titulo"
                                 variant="outlined"
                                 fullWidth
+                                name="titulo"
+                                value={libro.titulo}
+                                onChange={handleChange}
                                 />
                             </Grid>
                             <Grid item md={6} xs={12} className={classes.gridmb}>
@@ -36,6 +76,9 @@ const Libro = () => {
                                 label="Autor"
                                 variant="outlined"
                                 fullWidth
+                                name="autor"
+                                value={libro.autor}
+                                onChange={handleChange}
                                 />
                             </Grid>
                             <Grid item md={12} xs={12} className={classes.gridmb}>
@@ -43,7 +86,8 @@ const Libro = () => {
                                 variant="contained"
                                 fullWidth
                                 color="primary"
-                                type="submit">
+                                type="submit"
+                                onClick={guardarData}>
                                     Guardar
                                 </Button>
                             </Grid>
@@ -52,6 +96,41 @@ const Libro = () => {
                 </Card>
                </Grid>
             </Grid> 
+            <TableContainer component={Paper} className={classes.containermt}>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Categoría</TableCell>
+                            <TableCell>Titulo</TableCell>
+                            <TableCell>Autor</TableCell>
+                            <TableCell align="center" colSpan={2}>Acciones</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        <TableRow>
+                            <TableCell>Programacion</TableCell>
+                            <TableCell>React Avanzado</TableCell>
+                            <TableCell>Vaxi Drez</TableCell>
+                            <TableCell>
+                                <Button
+                                variant="contained"
+                                color="primary"
+                                onClick={abrirDialog}>
+                                    Editar
+                                </Button>
+                            </TableCell>
+                            <TableCell>
+                                <Button
+                                variant="contained"
+                                color="secondary"
+                                onClick={eliminarData}>
+                                    Eliminar
+                                </Button>
+                            </TableCell>
+                        </TableRow>
+                    </TableBody>
+                </Table>
+            </TableContainer>
        </Container>
     );
 };
